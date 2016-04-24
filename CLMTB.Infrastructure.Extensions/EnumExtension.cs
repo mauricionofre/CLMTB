ed5e2aa1284a8610ee5b44fb78ce659a65078588
mauricionofre <mauricionofre@hotmail.com>
2016-04-24 16:16:52 -0300

    Add Category Partial

---
+++ CLMTB.Infrastructure.Extensions/EnumExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLMTB.Infrastructure.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string GetEnumDescription(this Enum value)
+        {
+            var fi = value.GetType().GetField(value.ToString());
+            var attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes.Length > 0)
+                return ((DescriptionAttribute)attributes[0]).Description;
+            else
+                return value.ToString();
+        }
+
+        // the method to go from string->enum
+        public static T GetEnumFromDescription<T>(string stringValue)
+            where T : struct
+        {
+            foreach (object e in Enum.GetValues(typeof(T)))
+                if (GetEnumDescription((Enum)e).Equals(stringValue))
+                    return (T)e;
+            throw new ArgumentException("No matching enum value found.");
+        }
+
+        // and a method to get a list of string values - no KeyValuePair needed
+        public static IEnumerable<string> GetEnumDescriptions(Type enumType)
+        {
+            var strings = new Collection<string>();
+            foreach (Enum e in Enum.GetValues(enumType))
+                strings.Add(GetEnumDescription(e));
+            return strings;
+        }
+    }
+}
---
+++ CLMTB.Presentation.WinForm/Controls/AthleteForm/AthleteDialog.cs
@@ -0,0 +1,110 @@
+﻿using CLMTB.ApplicationLayer.DTO;
+using CLMTB.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLMTB.Presentation.WinForm.Controls.AthleteForm
+{
+    public partial class AthleteDialog : Form
+    {
+        public AthleteDialog()
+        {
+            InitializeComponent();
+            cbBloodGroup.DataSource = EnumHelper.GetEnumDescriptions(typeof(BloodGroupEnum));
+        }
+
+        private AthleteDTO _athlete;
+
+        public AthleteDTO Athlete
+        {
+            get { return _athlete; }
+            set
+            {
+                _athlete = value;
+
+                txtId.Text = _athlete.Id.ToString();
+                txtName.Text = _athlete.Name;
+                txtCPF.Text = _athlete.CPF;
+                txtRG.Text = _athlete.RG;
+                txtBirthDate.Text = _athlete.BirthDate.ToString("dd/MM/yyyy");
+                txtAge.Text = MathAge(_athlete.BirthDate).ToString();
+
+                cbBloodGroup.SelectedItem = EnumHelper.GetEnumDescription(_athlete.BloodType);
+
+                if (_athlete.Gender == null)
+                    rbMale.Checked = true;
+                else
+                    rbMale.Checked = _athlete.Gender.Equals("M");
+
+                txtEmail.Text = _athlete.Email;
+                txtFone.Text = _athlete.Fone;
+
+                txtStreet.Text = _athlete.Street;
+                txtNumber.Text = _athlete.Number;
+                txtNeighborhood.Text = _athlete.Neighborhood;
+                txtCity.Text = _athlete.City;
+                txtState.Text = _athlete.State;
+            }
+        }
+
+        private int MathAge(DateTime birthDate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate > today.AddYears(-age))
+                age--;
+
+            if (age == -1)
+                age = 0;
+
+            return age;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Athlete.Id = Convert.ToInt32(txtId.Text);
+            Athlete.Name = txtName.Text;
+            Athlete.CPF = txtCPF.TextNoFormatting();
+            Athlete.RG = txtRG.Text;
+
+            Athlete.BirthDate = DateTime.Parse(txtBirthDate.Text);
+            Athlete.Gender = rbMale.Checked ? "M" : "F";
+            Athlete.BloodType = EnumHelper.GetEnumFromDescription<BloodGroupEnum>(cbBloodGroup.Text);
+            Athlete.Email = txtEmail.Text;
+            Athlete.Fone = txtFone.Text;
+
+            Athlete.Street = txtStreet.Text;
+            Athlete.Neighborhood = txtNeighborhood.Text;
+            Athlete.Number = txtNumber.Text;
+            Athlete.City = txtCity.Text;
+            Athlete.State = txtState.Text;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtBirthDate_Validating(object sender, CancelEventArgs e)
+        {
+            DateTime dt;
+            DateTime.TryParse(txtBirthDate.Text, out dt);
+            txtAge.Text = MathAge(dt).ToString();
+            txtBirthDate.Text = dt.ToString("dd/MM/yyyy");
+        }
+
+        private void txtBirthDate_Enter(object sender, EventArgs e)
+        {
+            txtBirthDate.SelectAll();
+        }
+    }
+}
